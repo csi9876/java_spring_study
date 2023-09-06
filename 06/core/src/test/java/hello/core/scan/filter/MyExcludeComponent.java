acc8af6d120e19e713940bda25aeffa17ab6e2b0
최상익 <qjxltm@naver.com>
2023-09-06 14:56:49 +0900

    230906_필터

---
+++ 06/core/src/test/java/hello/core/scan/filter/MyExcludeComponent.java
@@ -0,0 +1,14 @@
+package hello.core.scan.filter;
+
+//import java.lang.annotation.Target;
+
+import java.lang.annotation.*;
+
+// 타입아니면 필드에 붙나 어디에 붙나 확인
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyExcludeComponent {
+}
+
+
