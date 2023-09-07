4a889cd9e2252bfe745a7e6514bad44a944f6bfd
최상익 <qjxltm@naver.com>
2023-09-07 14:06:25 +0900

    230907

--- 06/core/src/main/java/hello/core/AutoAppConfig.java
+++ 06/core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -10,4 +13,9 @@ import org.springframework.context.annotation.FilterType;
 ) // 스프링 빈 자동 등록, Configuration 붙은 건 제외
 public class AutoAppConfig {
 
+    // 수동 빈 등록과 자동 빈 등록이 충돌하면 수동 빈이 우선권을 가진다 > 이제는 에러나게 만들어줬음
+//    @Bean(name="memoryMemberRepository")
+//    MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
 }
--- 06/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ 06/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -29,6 +29,19 @@ public class OrderServiceImpl implements OrderService{
         this.discountPolicy = discountPolicy;
     }
 
+//     수정자 사용(setter)
+//    private MemberRepository memberRepository;
+//    private DiscountPolicy discountPolicy;
+//    @Autowired
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+//    @Autowired
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//        this.discountPolicy = discountPolicy;
+//    }
+
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
